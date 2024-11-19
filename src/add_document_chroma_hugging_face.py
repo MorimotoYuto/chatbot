ab1d64be25420ac6462340ff005b1717d81cd27e
もり <morimoto_yuto@outlook.jp>
2024-11-20 03:25:30 +0900

    Update add_document_chroma_hugging_face.py

--- src/add_document_chroma_hugging_face.py
+++ src/add_document_chroma_hugging_face.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import UnstructuredFileLoader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_chroma import Chroma
-from langchain_huggingface import HuggingFaceEmbeddings  # 修正済み
+from langchain_huggingface import HuggingFaceEmbeddings  
 import nltk
 
 nltk.download('punkt_tab')
@@ -14,31 +14,11 @@ load_dotenv()
 def initialize_vectorstore():
 
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-    vectorstore = Chroma(embedding_function=embeddings,persist_directory="../data/DB/vectorstore")  
+    vectorstore = Chroma(embedding_function=embeddings,persist_directory="../DB/vectorstore")  
     return vectorstore
 
 if __name__ == "__main__":
-"""    # サンプルテキスト
-    text = """生成AIは、近年急速に発展している技術の一つであり、多くの分野で注目を集めています。
-    生成AIは、機械学習の手法を用いて、新しいコンテンツを生成する能力を持っています。
-    特に、自然言語処理（NLP）や画像生成などの領域でその能力を発揮しています。
-    例えば、GPTシリーズやDALL-Eなどのモデルは、テキストや画像を生成するために大量のデータを学習し、その結果として非常にリアルで創造的なアウトプットを提供します。"""
 
-    # CharacterTextSplitterを初期化
-    text_splitter = CharacterTextSplitter(
-        chunk_size=15,         # 各チャンクの最大文字数を100に設定
-        chunk_overlap=10,       # 10文字の重複を設定
-        separator="\n"          # 改行で区切り
-    )
-
-    # テキストを分割
-    chunks = text_splitter.split_text(text)
-
-    # チャンクの内容を確認
-    for i, chunk in enumerate(chunks):
-        print(f"チャンク {i+1}:")
-        print(chunk)
-        print("\n---\n")"""
 
     
     file_path = sys.argv[1]
